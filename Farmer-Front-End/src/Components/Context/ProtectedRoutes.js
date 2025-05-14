d12b5c6caa7bb8f6d62cb81d509bdc090f7bfe06
AnshulMenaria <anshulmenaria@gmail.com>
2025-05-14 16:23:41 +0530

    Files Updation

--- Farmer-Front-End/src/Components/Context/ProtectedRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const farmerToken = localStorage.getItem("farmerToken");
-
-  return farmerToken ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
