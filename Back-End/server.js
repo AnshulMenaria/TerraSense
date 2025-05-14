2787b18f1a6402d0edbe7b9266135e1d498026d1
AnshulMenaria <anshulmenaria@gmail.com>
2025-05-14 16:09:03 +0530

    Docker FIle ANd nginx

---
+++ Back-End/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const routes = require("./Routes/Routes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("DB Connection Error:", err));
+
+// API Routes
+app.use("/api", routes);
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle React routing (for SPA)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
